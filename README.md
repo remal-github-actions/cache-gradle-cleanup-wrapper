67a5209d842e9eff0eb7e3c0179b8fab3a8796da
Semyon Levin <levin.semen@gmail.com>
2022-09-06 18:47:31 -0700

    Action code

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# template-typescript
-Template repository for GitHub typescript actions
+# cache-gradle-cleanup-wrapper
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import * as core from '@actions/core'
+import * as glob from '@actions/glob'
+import * as rimraf from 'rimraf'
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 async function run(): Promise<void> {
     try {
-        core.info('action logic')
+        let gradleUserHome = process.env.GRADLE_USER_HOME || '~/.gradle'
+        const globber = await glob.create(gradleUserHome + '/wrapper/dists/*/*/*.zip')
+        for await (const file of globber.globGenerator()) {
+            core.info(`Removing ${file}`)
+            rimraf.sync(file, {
+                disableGlob: true
+            })
+        }
 
     } catch (error) {
         core.setFailed(error instanceof Error ? error : (error as object).toString())
